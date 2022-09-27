3a47bfb749b6f84ca8f7d652402c65c5f82a8860
alishalistyaa <alisha.listya@gmail.com>
2022-09-27 07:38:09 +0700

    fix bugs on 'scan file matriks'

--- basreng.java
+++ basreng.java
@@ -1,32 +1,13 @@
 // Import
-import java.util.*;
+// import java.util.*;
 
 public class basreng {
     public static void main(String[] args) {
-        // Atribut
-        Scanner in = new Scanner (System.in);
-        int a , b, x;
 
         // Method
         matriks M2 = new matriks();
 
-        a = in.nextInt();
-        b = in.nextInt();
-
-        M2.bacaMatriks(a , b);
+        M2.bacaFileMatriks("file.txt");
         M2.tulisMatriks();
-        System.out.print("\n");
-
-        M2 = operasiMatriks.gauss(M2);
-        M2.tulisMatriks();
-        System.out.print("\n");
-
-        SPL.solveSPL(M2);
-
-        x = SPL.checkSPL(M2);
-        System.out.println(x);
-
-        // Menutup scanner
-        in.close();
     }
 }
